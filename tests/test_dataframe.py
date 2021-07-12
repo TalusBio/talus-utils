d3e17bd270d72ba25252d9b77ecbfd5f925204c3
Rico Meinl <rmeinl97@gmail.com>
2021-07-12 18:26:33 +0200

    New feature: sort_by_row_value decorator

--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -1,6 +1,7 @@
 """tests/test_dataframe.py"""
 import numpy as np
 import pandas as pd
+import pytest
 
 from pandas.testing import assert_frame_equal
 
@@ -89,7 +90,7 @@ def test_log_scaling() -> None:
 def test_log_scaling_custom_log() -> None:
     """Tests the log_scaling decorator with a custom log function (np.log2)."""
     df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
-    df_expected = np.log2(df_input)
+    df_expected = np.log2(df_input.where(df_input >= 1))
 
     df_actual = dataframe.log_scaling(log_function=np.log2)(dummy_function)(df_input)
     assert_frame_equal(df_actual, df_expected)
@@ -127,3 +128,191 @@ def test_pivot_table() -> None:
         dummy_function
     )(df_input)
     assert_frame_equal(df_actual, df_expected)
+
+
+def test_normalize_value_error() -> None:
+    """Tests the normalize decorator with a value error."""
+    df_input = pd.DataFrame([{"test": "a", "test2": "b"}, {"test": "c", "test2": "d"}])
+
+    with pytest.raises(ValueError):
+        _ = dataframe.normalize(how="nonexisting")(dummy_function)(df_input)
+
+
+def test_normalize_minmax() -> None:
+    """Tests the normalize decorator with how='minmax'."""
+    df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
+    df_expected = (df_input - df_input.min()) / (df_input.max() - df_input.min())
+
+    df_actual = dataframe.normalize(how="minmax")(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_expected)
+
+
+def test_normalize_row() -> None:
+    """Tests the normalize decorator with how='row'."""
+    df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
+    df_expected = df_input.apply(lambda x: x / x.sum(), axis=1)
+
+    df_actual = dataframe.normalize(how="row")(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_expected)
+
+
+def test_normalize_column() -> None:
+    """Tests the normalize decorator with how='column'."""
+    df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
+    df_expected = df_input.apply(lambda x: x / x.sum(), axis=0)
+
+    df_actual = dataframe.normalize(how="column")(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_expected)
+
+
+def test_sort_row_values_value_error() -> None:
+    """Tests the sort_by decorator with a value error."""
+    df_input = pd.DataFrame([{"test": "a", "test2": "b"}, {"test": "c", "test2": "d"}])
+
+    with pytest.raises(ValueError):
+        _ = dataframe.sort_row_values(how="nonexisting")(dummy_function)(df_input)
+
+
+def test_sort_row_values_max() -> None:
+    """Tests the sort_row_values decorator with 'max'."""
+    df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
+    df_abs_expected = df_input.reindex(
+        index=df_input.abs().max(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(
+        how="max", use_absolute_values=True, sort_ascending=True
+    )(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_abs_expected)
+
+    df_expected = df_input.reindex(
+        index=df_input.max(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="max", sort_ascending=True)(
+        dummy_function
+    )(df_input)
+    assert_frame_equal(df_actual, df_expected)
+
+    df_desc_expected = df_input.reindex(
+        index=df_input.max(axis=1).sort_values(ascending=False).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="max")(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_desc_expected)
+
+
+def test_sort_row_values_min() -> None:
+    """Tests the sort_row_values decorator with 'min'."""
+    df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
+    df_abs_expected = df_input.reindex(
+        index=df_input.abs().min(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(
+        how="min", use_absolute_values=True, sort_ascending=True
+    )(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_abs_expected)
+
+    df_expected = df_input.reindex(
+        index=df_input.min(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="min", sort_ascending=True)(
+        dummy_function
+    )(df_input)
+    assert_frame_equal(df_actual, df_expected)
+
+    df_desc_expected = df_input.reindex(
+        index=df_input.min(axis=1).sort_values(ascending=False).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="min")(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_desc_expected)
+
+
+def test_sort_row_values_median() -> None:
+    """Tests the sort_row_values decorator with 'median'."""
+    df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
+    df_abs_expected = df_input.reindex(
+        index=df_input.abs().median(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(
+        how="median", use_absolute_values=True, sort_ascending=True
+    )(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_abs_expected)
+
+    df_expected = df_input.reindex(
+        index=df_input.median(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="median", sort_ascending=True)(
+        dummy_function
+    )(df_input)
+    assert_frame_equal(df_actual, df_expected)
+
+    df_desc_expected = df_input.reindex(
+        index=df_input.median(axis=1).sort_values(ascending=False).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="median")(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_desc_expected)
+
+
+def test_sort_row_values_mean() -> None:
+    """Tests the sort_row_values decorator with 'mean'."""
+    df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
+    df_abs_expected = df_input.reindex(
+        index=df_input.abs().mean(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(
+        how="mean", use_absolute_values=True, sort_ascending=True
+    )(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_abs_expected)
+
+    df_expected = df_input.reindex(
+        index=df_input.mean(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="mean", sort_ascending=True)(
+        dummy_function
+    )(df_input)
+    assert_frame_equal(df_actual, df_expected)
+
+    df_desc_expected = df_input.reindex(
+        index=df_input.mean(axis=1).sort_values(ascending=False).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="mean")(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_desc_expected)
+
+
+def test_sort_row_values_sum() -> None:
+    """Tests the sort_row_values decorator with 'sum'."""
+    df_input = pd.DataFrame(np.random.rand(5, 5) * 100)
+    df_abs_expected = df_input.reindex(
+        index=df_input.abs().sum(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(
+        how="sum", use_absolute_values=True, sort_ascending=True
+    )(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_abs_expected)
+
+    df_expected = df_input.reindex(
+        index=df_input.sum(axis=1).sort_values(ascending=True).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="sum", sort_ascending=True)(
+        dummy_function
+    )(df_input)
+    assert_frame_equal(df_actual, df_expected)
+
+    df_desc_expected = df_input.reindex(
+        index=df_input.sum(axis=1).sort_values(ascending=False).index
+    )
+
+    df_actual = dataframe.sort_row_values(how="sum")(dummy_function)(df_input)
+    assert_frame_equal(df_actual, df_desc_expected)
